b4b9a4a2e6820670ff0054b6fef3ab50514a637f
grulex <god@vk.com>
2023-10-10 12:51:53 +0100

    Bot init message

--- bot/bot.go
+++ bot/bot.go
@@ -79,7 +79,8 @@ func (s TelegramBot) Start() error {
 				msg := tgbotapi.NewMessage(update.MyChatMember.Chat.ID,
 					"Hello, I'm Wishlist Bot!\n\nI can help you to manage your wishlist.\n\n"+
 						"Press \"Create Wishlist!\" or \"My Wishlist\" menu.\n\n"+
-						"Also, you can type @"+s.telegramBot.Self.UserName+" and your username in any chat and I'll share your wishlist.")
+						"Also, you can type @"+s.telegramBot.Self.UserName+" and your username in "+
+						"any chat and I'll share your wishlist.")
 				msg.ReplyMarkup = &button
 				_, err := s.telegramBot.Send(msg)
 				if err != nil {

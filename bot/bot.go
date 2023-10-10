a36215de7972c346b48aa19e035781a7c6a05280
grulex <god@vk.com>
2023-10-10 12:51:01 +0100

    Bot init message

--- bot/bot.go
+++ bot/bot.go
@@ -44,32 +44,50 @@ func (s TelegramBot) Start() error {
 	updateConfig.Timeout = 30
 	updates := s.telegramBot.GetUpdatesChan(updateConfig)
 	for update := range updates {
-		if update.InlineQuery == nil {
-			continue
+		if update.InlineQuery != nil {
+			inlineMessage := update.InlineQuery.Query
+			inlineMessage = strings.Trim(inlineMessage, " @")
+			if inlineMessage == "" {
+				continue
+			}
+			wishlist, err := s.wishlistService.Get(context.Background(), wishlistPkg.ID(inlineMessage))
+			if err != nil {
+				continue
+			}
+
+			postText := wishlist.Title + "\n\n" + wishlist.Description
+			article := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID, wishlist.Title, postText)
+			button := getButton("Open Wishlist", s.miniAppUrl+"?startapp="+string(wishlist.ID))
+			article.ReplyMarkup = &button
+			article.Description = wishlist.Description
+			article.ThumbURL = "https://png.pngtree.com/png-vector/20221121/ourmid/pngtree-comicstyle-wishlist-icon-with-splash-effect-health-sign-add-vector-png-image_41870708.jpg"
+
+			answer := tgbotapi.InlineConfig{
+				InlineQueryID: update.InlineQuery.ID,
+				IsPersonal:    true,
+				Results:       []interface{}{article},
+			}
+			_, err = s.telegramBot.Send(answer)
+			if err != nil && err.Error() != "json: cannot unmarshal bool into Go value of type tgbotapi.Message" {
+				log.Println(err)
+			}
 		}
 
-		inlineMessage := update.InlineQuery.Query
-		inlineMessage = strings.Trim(inlineMessage, " ")
-		if inlineMessage == "" {
-			continue
-		}
-		wishlist, err := s.wishlistService.Get(context.Background(), wishlistPkg.ID(inlineMessage))
-		if err != nil {
-			continue
+		if update.MyChatMember != nil {
+			if update.MyChatMember.NewChatMember.Status == "member" {
+				button := getButton(" 🎁Create Wishlist!", s.miniAppUrl)
+				msg := tgbotapi.NewMessage(update.MyChatMember.Chat.ID,
+					"Hello, I'm Wishlist Bot!\n\nI can help you to manage your wishlist.\n\n"+
+						"Press \"Create Wishlist!\" or \"My Wishlist\" menu.\n\n"+
+						"Also, you can type @"+s.telegramBot.Self.UserName+" and your username in any chat and I'll share your wishlist.")
+				msg.ReplyMarkup = &button
+				_, err := s.telegramBot.Send(msg)
+				if err != nil {
+					log.Println(err)
+				}
+			}
 		}
 
-		article := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID, wishlist.Title, wishlist.Description)
-
-		button := getButton("Open Wishlist", s.miniAppUrl+"?startapp="+string(wishlist.ID))
-		article.ReplyMarkup = &button
-		answer := tgbotapi.InlineConfig{
-			InlineQueryID: update.InlineQuery.ID,
-			Results:       []interface{}{article},
-		}
-		_, err = s.telegramBot.Send(answer)
-		if err != nil {
-			log.Println(err)
-		}
 	}
 	return nil
 }
